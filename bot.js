fe362dcababa4583b29fa4deb891b59a2e1cf041
djalobre <giallobre@gmail.com>
2020-08-04 17:25:02 +0200

    make it better

---
+++ bot.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+
+const uvrede = ['braaaavo majmune \:7190_linkpepehype:','aj cuti \:FeelsBanMan:','e aj samo ti ne seri \:PepeYikes:']
+
+
+
+client.on('message', user => {
+    if (user.author.username === 'djalobre') {
+      const random = Math.floor(Math.random() * uvrede.length);
+      user.reply(uvrede[random]);
+    }
+  });
