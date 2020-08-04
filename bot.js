dcf43c15e85a091f6acb657ce3d32a5c71caa432
djalobre <giallobre@gmail.com>
2020-08-05 00:11:46 +0200

    fixed custom emojis added users

--- bot.js
+++ bot.js
@@ -10,12 +10,12 @@ client.on('ready', () => {
 client.login(process.env.DISCORD_TOKEN);
 
 
-const uvrede = ['braaaavo majmune <:PepeYikes:637575565528006656>','aj cuti <:PepeYikes:637575565528006656>','e aj samo ti ne seri <:PepeYikes:637575565528006656>']
+const uvrede = ['braaaavo majmune <:PepeYikes:637575565528006656>','aj cuti <:pepeGun:501928360914452511>','e aj samo ti ne seri <:PeepoPing:637575565574144011>']
 
 
 
 client.on('message', user => {
-    if (user.author.username === 'Brle' || user.author.username === 'djalobre' ) {
+    if (user.author.username === 'Brle' || user.author.username === 'Dajnfehr' || user.author.username === 'bno2' ) {
       const random = Math.floor(Math.random() * uvrede.length);
       user.reply(uvrede[random]);
     }
